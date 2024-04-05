19c24b56b61032ff42be661e6fdb988070ab328d
Eallekors <erkxalle@gmail.com>
2024-04-05 09:55:58 +0300

    UL 4

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import MealItem from './MealItem'; // Import the MealItem component
 
 const Meals = () => {
     const [meals, setMeals] = useState([]);
@@ -7,14 +8,19 @@ const Meals = () => {
         const fetchMeals = async () => {
             const response = await fetch('http://localhost:3001/meals');
             const mealsData = await response.json();
-            console.log(mealsData); 
             setMeals(mealsData);
         };
 
         fetchMeals();
     }, []);
 
-    return null; 
+    return (
+        <ul id="meals" >
+            {meals.map(meal => (
+                <MealItem key={meal.id} meal={meal} /> // Pass meal data as props to MealItem component
+            ))}
+        </ul>
+    );
 };
 
 export default Meals;
