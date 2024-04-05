aeb4ad48593de284bd37ec978419e6d2080e6880
Eallekors <erkxalle@gmail.com>
2024-04-05 09:48:13 +0300

    Ul 3 fix

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -5,23 +5,16 @@ const Meals = () => {
 
     useEffect(() => {
         const fetchMeals = async () => {
-            try {
-                const response = await fetch('http://localhost:3001/meals');
-                if (!response.ok) {
-                    throw new Error('Failed to fetch meals');
-                }
-                const mealsData = await response.json();
-                console.log(mealsData); // Log meal data to console
-                setMeals(mealsData);
-            } catch (error) {
-                console.error('Error fetching meals:', error);
-            }
+            const response = await fetch('http://localhost:3001/meals');
+            const mealsData = await response.json();
+            console.log(mealsData); 
+            setMeals(mealsData);
         };
 
         fetchMeals();
     }, []);
 
-    return null; // Render nothing in the UI
+    return null; 
 };
 
 export default Meals;
