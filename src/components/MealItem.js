e32a441ba9ba56bc05d982f9591503e789b621af
Eallekors <erkxalle@gmail.com>
2024-04-05 10:26:21 +0300

    UL 7

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,8 +1,16 @@
+import React, { useContext } from 'react';
 import Button from "./UI/Button";
+import { CartContext } from "../store/CartContext";
 
 const MealItem = (props) => {
-
     const formattedPrice = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(props.meal.price);
+
+    const { addItem } = useContext(CartContext); // Use useContext hook to access addItem function from CartContext
+
+    const handleAddToCart = () => {
+        addItem(props.meal); // Call addItem function to add item to the cart
+        console.log(props.meal);
+    };
 
     return (
         <li className="meal-item">
@@ -14,7 +22,7 @@ const MealItem = (props) => {
                     <p className="meal-item-description">{props.meal.description}</p>
                 </div>
                 <p>
-                <Button onClick={() => console.log('Add to Cart clicked')}>Add to Cart</Button>
+                <Button onClick={handleAddToCart}>Add to Cart</Button>
                </p>
             </article>
         </li>
---
+++ src/store/CartContext.js
@@ -0,0 +1,20 @@
+// CartContext.js
+import React, { createContext, useState } from 'react';
+
+const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItem = (item) => {
+        setCartItems(prevItems => [...prevItems, item]); // Push item into the cartItems array
+    };
+
+    return (
+        <CartContext.Provider value={{ cartItems, addItem }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
+
+export { CartContext, CartProvider };
